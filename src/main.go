28d640f34ea5ee2d3dca367d9020b85e8bfd8aa7
root <root@debian.localdomain>
2024-04-21 13:00:08 +0200

    création du localhost a l'execution

--- src/main.go
+++ src/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	menu "src/Controller"
 )
 
 func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Bienvenue sur le serveur de réservation !")
+	})
+
+	fmt.Println("http://localhost:8085")
+	err := http.ListenAndServe(":8085", nil)
+	if err != nil {
+		fmt.Println("Erreur lors du démarrage du serveur:", err)
+	}
+
 	menu.DisplayMenu()
+
 }
