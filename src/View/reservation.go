e26b4fd694401333f7b611d21f99eab1bd9864a4
victor <victor.dalet>
2024-04-22 18:03:49 +0200

    feat(web): Add reservation table in web View

--- src/View/reservation.go
+++ src/View/reservation.go
@@ -6,7 +6,7 @@ import (
 )
 
 func DisplayReservations(reservations []Types.Reservation) {
-	fmt.Println("ID\tRoom ID\tDate\tStart Time\tEnd Time")
+	fmt.Println("ID\tRoom ID\tStart Time\tEnd Time")
 	for _, r := range reservations {
 		fmt.Println(r.Id, r.RoomId, r.StartTime, r.EndTime)
 	}
--- src/ViewWeb/handler/view_reservations_handler.go
+++ src/ViewWeb/handler/view_reservations_handler.go
@@ -3,12 +3,14 @@ package handler
 import (
 	"html/template"
 	"net/http"
+	"src/Repository"
 )
 
 func ViewReservationsHandler(w http.ResponseWriter, r *http.Request) {
+	reservations := Repository.GetReservations()
 	tmpl, err := template.ParseFiles("ViewWeb/templates/viewReservations.html")
 
-	err = tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, reservations)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
