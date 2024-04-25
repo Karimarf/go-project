79972bd5f371c6d1713682d1baab577fb6ac4476
root <root@debian.localdomain>
2024-04-25 19:01:30 +0200

    Final version

--- src/ViewWeb/handler/create_reservation_handler.go
+++ src/ViewWeb/handler/create_reservation_handler.go
@@ -9,37 +9,19 @@ import (
 
 func CreateReservationHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		tmpl, err := template.ParseFiles("ViewWeb/templates/createReservation.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+		tmpl, _ := template.ParseFiles("ViewWeb/templates/createReservation.html")
+		tmpl.Execute(w, nil)
 	} else if r.Method == "POST" {
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
+		r.ParseForm()
 		roomId := r.Form.Get("roomId")
 		startTime := r.Form.Get("startTime")
 		endTime := r.Form.Get("endTime")
 
-		roomIdStr, err := strconv.Atoi(roomId)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		reservation.CreateReservation(roomIdStr, startTime, endTime)
+		roomIdInt, _ := strconv.Atoi(roomId)
+		reservation.CreateReservation(roomIdInt, startTime, endTime)
 
 		http.Redirect(w, r, "/createReservation", http.StatusSeeOther)
 	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
 	}
 }
--- src/ViewWeb/handler/create_room_handler.go
+++ src/ViewWeb/handler/create_room_handler.go
@@ -9,44 +9,21 @@ import (
 
 func CreateRoomHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		// Afficher le formulaire de création de chambre
-		tmpl, err := template.ParseFiles("ViewWeb/templates/createRoom.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	} else if r.Method == "POST" {
-		// Analyser les données du formulaire
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
+		tmpl, _ := template.ParseFiles("ViewWeb/templates/createRoom.html")
+		tmpl.Execute(w, nil)
 
-		// Récupérer les données du formulaire
+	} else if r.Method == "POST" {
+		r.ParseForm()
+
 		name := r.Form.Get("name")
 		capacityStr := r.Form.Get("capacity")
 		description := r.Form.Get("description")
+		capacity, _ := strconv.Atoi(capacityStr)
 
-		// Convertir la capacité de chaîne de caractères en un entier
-		capacity, err := strconv.Atoi(capacityStr)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		// Appeler la fonction CreateRoom avec les données du formulaire
 		reservation.CreateRoom(name, capacity, description)
 
-		// Rediriger vers la page de création de chambre
 		http.Redirect(w, r, "/createRoom", http.StatusSeeOther)
 	} else {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
 	}
 }
